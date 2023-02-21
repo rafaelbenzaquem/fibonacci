7e5da2f9f97c4f89bc27c34c45b0b16a86afca23
Rafael Benzaquem Neto <rafael.neto@outlook.com>
2023-02-21 14:27:55 -0300

    feat: função recursiva que retorna o n-ésimo termo da sequência de fibonacci

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+int fibonacciRecursivo(int n);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cout << "digite o valor do termo da funcao fibonacci:";
+    cin >> n;
+    int f = fibonacciRecursivo(n);
+    cout << "\nfibonacci(" << n << ")=" << f << endl;
     return 0;
+}
+
+// função recursiva para calcular o n-ésimo termo da sequência de fibonacci, complexidade exponêncial 2^n
+int fibonacciRecursivo(int n){
+    if(n<=1){
+        return n;
+    }
+    return fibonacciRecursivo(n-1) + fibonacciRecursivo(n-2);
 }
